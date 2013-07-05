8a34ca5670b12a1f73616375d54ee29a4fe99da4
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-07-05 07:52:39 +0200

    search input boxes

--- src/clj/rdfcat/server.clj
+++ src/clj/rdfcat/server.clj
@@ -6,16 +6,15 @@
             [net.cgrand.enlive-html :as html :refer [deftemplate]])
   (:gen-class))
 
-(deftemplate anne-lena "p1.html" [overskrift]
-  [:h2] (html/content overskrift))
+(deftemplate anne-lena "p1.html" []
+  [:h2] (html/content "Utforsk Deichmanske biblioteks katalog"))
 
-(deftemplate petter "p2.html" [overskrift]
-  [:h2] (html/content overskrift))
+(deftemplate petter "p2.html" [] )
 
 (defroutes app-routes
   (GET "/" [] (redirect "p1"))
-  (GET "/p1" [] (anne-lena "Prototype \"Anne-Lena\""))
-  (GET "/p2" [] (petter "Prototype \"Petter\""))
+  (GET "/p1" [] (anne-lena))
+  (GET "/p2" [] (petter))
   (route/resources "/")
   (route/not-found "Not Found."))
 
