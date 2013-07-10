beaa2ca0e4335271d709dbe7d01d74e617d26b79
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-07-10 14:26:11 +0200

    helper to count how many subject labels to show

--- src/clj/rdfcat/util/string.clj
+++ src/clj/rdfcat/util/string.clj
@@ -1,5 +1,5 @@
 (ns rdfcat.util.string
-  "String transformation functions")
+  "String transformation and utility functions")
 
 (defn date-clean [s]
   "Takes a string and return a 4-digit year as integer,
@@ -10,3 +10,13 @@
    (if (zero? cleaned)
      nil
      cleaned)))
+
+(defn length-num
+  "Given a collection of strings and a maximum character length,
+  returns how many of the elements in coll needed to reach length. "
+  [coll length]
+  (loop [c (map count coll) s 0 i 0]
+    (cond
+      (> s length) (dec i)
+      (empty? c) i
+      :else (recur (rest c) (+ (first c) s) (inc i)))))
--- test/rdfcat/string_test.clj
+++ test/rdfcat/string_test.clj
@@ -11,3 +11,8 @@
        "19-"       1900
        "1895-1979" 1895
        "p1994"     1994))
+
+(deftest test-length-num
+  (is (= 2 (length-num ["kake" "mann" "fisk"] 10)))
+  (is (= 3 (length-num ["kake" "mann" "fisk"] 100)))
+  (is (= 6 (length-num ["a" "bb" "ccc" "ddd" "eee" "xxx" "yy"] 15))))
